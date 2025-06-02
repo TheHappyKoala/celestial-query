c8565291a822182e3c81475647bd85abb2256f72
Darrell Huffman <rather.glorious@gmail.com>
2025-06-02 19:31:27 +0200

    fix: remove logs

--- src/utils/parsing-utils.ts
+++ src/utils/parsing-utils.ts
@@ -1,18 +1,10 @@
 const sliceByLines = (string: string, start: number, end: number): string => {
-  console.log("string", string);
-  console.log("start", start);
-  console.log("end", end);
   const lines = string.split("\n");
-  console.log("lines", lines);
   const startFromLineNumber = start;
   const stopFromLineNumber = lines.length - end;
 
-  console.log("stopFromLineNumber", stopFromLineNumber);
-
   const linesToInclude = lines.slice(startFromLineNumber, stopFromLineNumber);
 
-  console.log("linesToInclude", linesToInclude);
-  console.log("Resulting string", linesToInclude.join("\n"));
   return linesToInclude.join("\n");
 };
 
