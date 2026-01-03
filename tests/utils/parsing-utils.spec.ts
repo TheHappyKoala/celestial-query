1819ff14e85a2953217731cd3c5355b2c31f533e
Darrell Huffman <rather.glorious@gmail.com>
2026-01-03 21:22:11 +0100

    feat: add parsing utils

--- tests/utils/parsing-utils.spec.ts
+++
@@ -1,9 +0,0 @@
-import { sliceByLines } from "src/utils/parsing-utils";
-
-describe("sliceByLines", () => {
-  it("should slice a string correctly given start and end line numbers", () => {
-    const input = "bob\nskips\nto\nthe\nnext\nline";
-
-    expect(sliceByLines(input, 1, 1)).toBe("skips\nto\nthe\nnext");
-  });
-});
