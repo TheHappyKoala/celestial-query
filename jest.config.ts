a50cfdffb481b7fd034c71e4a5cc82c71b33afa0
Darrell Huffman <rather.glorious@gmail.com>
2026-01-03 21:25:01 +0100

    chore: work on scaffolding

--- jest.config.ts
+++ jest.config.ts
@@ -7,8 +7,12 @@ const config: Config.InitialOptions = {
   },
   moduleNameMapper: {
     "src/(.*)": "<rootDir>/src/$1",
-    "tests/(.*)": "<rootDir>/__tests__/$1",
+    "tests/(.*)": "<rootDir>/tests/$1",
+    "types/(.*)": "<rootDir>/types/$1",
   },
+  testMatch: ["**/tests/**/*.spec.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/types/"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/types/"],
 };
 
 export default config;
